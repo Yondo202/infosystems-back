050ff032d84bb3786afa791e167198a2f82ca777
huug buug <yondooo61@gmail.com>
2021-06-22 23:45:43 +0800

    add url

--- plugins/wysiwyg/admin/src/components/CKEditor/index.js
+++ plugins/wysiwyg/admin/src/components/CKEditor/index.js
@@ -89,6 +89,11 @@ const Editor = ({ onChange, name, value }) => {
         editor={ClassicEditor}
         config={configuration}
         data={value}
+        onReady={(editor) => {
+          if (value) {
+             editor.setData(value);
+          }
+        }}
         onChange={(event, editor) => {
           const data = editor.getData();
           onChange({ target: { name, value: data } });
