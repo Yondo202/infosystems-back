558a6737fe9389bd193d5420ff8a4e8793bc90ab
infosystems-mn <contact@infosystems.mn>
2022-01-03 11:47:35 +0800

    hansh add

---
+++ api/bankrate/controllers/bankrate.js
@@ -0,0 +1,57 @@
+const axios = require("axios");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+
+module.exports = {
+  getRates: async (ctx) => {
+    const keys = [
+      "currency",
+      "belen_avah",
+      "belen_zarah",
+      "belen_bus_avah",
+      "belen_bus_zarah",
+    ];
+    const { bankcode } = ctx.request.query;
+    const currencies = [];
+    console.log(bankcode);
+    if (bankcode == undefined || bankcode == "mongol") {
+      //Монгол банк
+      const res = await axios.get(
+        "http://monxansh.appspot.com/xansh.json?currency=USD|EUR|CNY|RUB|GBP|KRW|JPY"
+      );
+      rows = res.data;
+
+      for (const row of rows) {
+        const temp = {};
+        temp.currency = row.code;
+        temp.belen_avah = row.rate_float;
+        temp.belen_zarah = row.rate_float;
+        temp.belen_bus_avah = 0;
+        temp.belen_bus_zarah = 0;
+
+        currencies.push(temp);
+      }
+    } else {
+      //Хаан банк
+      const res = await axios.get(
+        "https://www.khanbank.com/mn/home/ratesForSites/"
+      );
+      const html = res.data;
+      const { document } = new JSDOM(html).window;
+      const table = document.querySelector("tbody");
+      const rows = table.getElementsByTagName("tr");
+
+      for (const row of rows) {
+        const cells = row.getElementsByTagName("td");
+        let index = 0;
+        const temp = {};
+        for (const cell of cells) {
+          temp[keys[index]] = cell.textContent.trim();
+          index === 4 ? (index = 0) : index++;
+        }
+        currencies.push(temp);
+      }
+    }
+    ctx.send(currencies);
+  },
+};
